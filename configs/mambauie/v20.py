97054e35a6abf9797177c7a9ac9fb86bc46c14e6
weitunglin <weitung8@gmail.com>
2024-05-16 14:56:05 +0800

    init v20 and v21

--- configs/mambauie/v20.py
+++ configs/mambauie/v20.py
@@ -1,6 +1,6 @@
 """
-defaultdict(<class 'float'>, {'conv': 0.4230144, 'layer_norm': 0.62208, 'linear': 3.446338752, 'einsum': 1.49301, 'PythonOp.SelectiveScanFn': 2.1897264})
-params 110742 GFLOPs 8.174169552
+defaultdict(<class 'float'>, {'conv': 0.4230144, 'layer_norm': 0.62208, 'linear': 3.906693504, 'einsum': 1.29381, 'PythonOp.SelectiveScanFn': 1.8413208})
+params 117438 GFLOPs 8.086918703999999
 """
 
 _base_ = [
@@ -19,10 +19,11 @@ model = dict(
         type='MM_VSSM',
         depths=[1]*6,
         dims=[36]*6,
-        d_state=12,
-        biattn_act_ratio=0.125,
+        d_state=10,
+        biattn_act_ratio=0.25,
         ver='v16',
-    ))
+    ),
+    pixel_loss=dict(type='CharbonnierLoss'))
 
 batch_size = 32
 train_dataloader = dict(batch_size=batch_size)
@@ -31,14 +32,15 @@ val_dataloader = dict(batch_size=batch_size)
 optim_wrapper = dict(
     dict(
         type='AmpOptimWrapper',
-        optimizer=dict(type='AdamW', lr=0.0002, betas=(0.9, 0.999))))
+        optimizer=dict(type='AdamW', lr=0.00025, betas=(0.9, 0.999))))
 
 max_epochs = 800
 param_scheduler = [
     dict(
-        type='LinearLR', start_factor=0.001, by_epoch=True, begin=0,
-        end=15),
-    dict(type='CosineAnnealingLR', by_epoch=True, T_max=max_epochs+200, convert_to_iter_based=True, eta_min=1e-5),]
+        type='LinearLR', start_factor=0.001, by_epoch=True, begin=0, end=15),
+    dict(
+        type='LinearLR', start_factor=1, end_factor=0.5, by_epoch=True, begin=15, end=max_epochs//2),
+    dict(type='CosineAnnealingLR', by_epoch=True, begin=max_epochs//2, T_max=max_epochs, convert_to_iter_based=True),]
 
 train_cfg = dict(by_epoch=True, max_epochs=max_epochs)
 
@@ -48,4 +50,4 @@ visualizer = dict(
 auto_scale_lr = dict(enable=False)
 default_hooks = dict(logger=dict(interval=5))
 
-custom_hooks = [dict(type='BasicVisualizationHook', interval=3)]
+custom_hooks = [dict(type='BasicVisualizationHook', interval=4)]
--- configs/mambauie/v21.py
+++ configs/mambauie/v21.py
@@ -1,13 +1,13 @@
 """
-defaultdict(<class 'float'>, {'conv': 0.5640192, 'layer_norm': 0.82944, 'linear': 6.934173696, 'einsum': 1.45968, 'PythonOp.SelectiveScanFn': 1.9906536})
-params 183078 GFLOPs 11.777966496000001
+defaultdict(<class 'float'>, {'conv': 0.4230144, 'layer_norm': 0.62208, 'linear': 3.906693504, 'einsum': 1.29381, 'PythonOp.SelectiveScanFn': 1.8413208})
+params 117438 GFLOPs 8.086918703999999
 """
 
 _base_ = [
     './seamamba.py'
 ]
 
-ver = 'v16'
+ver = 'v21'
 experiment_name = f'seamamba_uieb_{ver}'
 work_dir = f'./work_dirs/{experiment_name}'
 save_dir = './work_dirs/'
@@ -18,10 +18,10 @@ model = dict(
     generator=dict(
         type='MM_VSSM',
         depths=[1]*6,
-        dims=[48]*6,
-        d_state=8,
+        dims=[36]*6,
+        d_state=10,
         biattn_act_ratio=0.25,
-        ver=ver,
+        ver='v16',
     ),
     pixel_loss=dict(type='CharbonnierLoss'))
 
@@ -32,14 +32,15 @@ val_dataloader = dict(batch_size=batch_size)
 optim_wrapper = dict(
     dict(
         type='AmpOptimWrapper',
-        optimizer=dict(type='AdamW', lr=0.00025, betas=(0.9, 0.999))))
+        optimizer=dict(type='AdamW', lr=0.00015, betas=(0.9, 0.999))))
 
 max_epochs = 800
 param_scheduler = [
     dict(
-        type='LinearLR', start_factor=0.001, by_epoch=True, begin=0,
-        end=15),
-    dict(type='CosineAnnealingLR', by_epoch=True, T_max=max_epochs-200, convert_to_iter_based=True, eta_min=1e-5),]
+        type='LinearLR', start_factor=0.001, by_epoch=True, begin=0, end=15),
+    dict(
+        type='LinearLR', start_factor=1, end_factor=0.5, by_epoch=True, begin=15, end=max_epochs//2),
+    dict(type='CosineAnnealingLR', by_epoch=True, begin=max_epochs//2, T_max=max_epochs, convert_to_iter_based=True),]
 
 train_cfg = dict(by_epoch=True, max_epochs=max_epochs)
 
@@ -49,4 +50,4 @@ visualizer = dict(
 auto_scale_lr = dict(enable=False)
 default_hooks = dict(logger=dict(interval=5))
 
-custom_hooks = [dict(type='BasicVisualizationHook', interval=3)]
+custom_hooks = [dict(type='BasicVisualizationHook', interval=4)]
