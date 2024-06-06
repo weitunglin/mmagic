16bff94505ceb3a22dd4168edccd9b96d88672fc
weitunglin <weitung8@gmail.com>
2024-06-06 14:22:52 +0800

    add results

--- configs/lwmamba/final.py
+++ configs/lwmamba/final.py
@@ -1,6 +1,51 @@
 """
-(v40)
 params 8.682 M, FLOPs 7.606 G
+
+iter 33200 uie/MAE: 0.0584  uie/MSE: 0.0061  uie/SSIM: 0.9215  uie/PSNR: 23.5876  data_time: 0.1315  time: 0.6182
+Start evalutaing T90
+PSNR:  23.26439414457024
+SSIM:  0.9084961349111366
+MSE:  0.006392932273526553
+UCIQE:  0.617646223794257
+UIQM:  3.0484763800040464
+NIQE:  5.581812865081653
+URanker: 2.215339424771567
+MUSIQ: 52.2026805029975
+Start evalutaing C60
+UCIQE:  0.5867716214313753
+UIQM:  2.8687242172755725
+NIQE:  6.46677250381111
+URanker: 1.5107721330287556
+MUSIQ: 48.025184567769365
+Start evalutaing UCCS
+UCIQE:  0.5618736255691569
+UIQM:  3.0532465552891996
+NIQE:  4.752065231873926
+URanker: 1.3774680538102984
+MUSIQ: 31.201950352986653
+
+iter 37650 uie/MAE: 0.0587  uie/MSE: 0.0062  uie/SSIM: 0.9236  uie/PSNR: 23.5332
+Start evalutaing T90
+PSNR:  23.20493805018966
+SSIM:  0.9101273139006643
+MSE:  0.006456596438282841
+UCIQE:  0.6183598856129596
+UIQM:  3.005769978421612
+NIQE:  5.57675808847364
+URanker: 2.209049194306135
+MUSIQ: 52.28248744540744
+Start evalutaing C60
+UCIQE:  0.5866200627564432
+UIQM:  2.7904716684743276
+NIQE:  6.511373322766993
+URanker: 1.506754536430041
+MUSIQ: 48.08399906158447
+Start evalutaing UCCS
+UCIQE:  0.5619259755651814
+UIQM:  3.037282983130936
+NIQE:  4.7152205458605705
+URanker: 1.4017370195810994
+MUSIQ: 31.422313912709555
 """
 
 _base_ = [
@@ -36,7 +81,7 @@ model = dict(
         conv_down=False,
         no_act_branch=True,
     ),
-    pixel_loss=dict(type='CharbonnierLoss', loss_weight=1.0, reduction='mean', eps=1e-10),
+    pixel_loss=dict(type='CharbonnierLoss', loss_weight=1.0, reduction='mean'),
     data_preprocessor=dict(
         type='DataPreprocessor',
         mean=[0., 0., 0.],
@@ -53,7 +98,7 @@ optim_wrapper = dict(
         type='AmpOptimWrapper',
         optimizer=dict(type='AdamW', lr=0.0004, betas=(0.9, 0.999), weight_decay=0.5)))
 
-max_epochs = 500
+max_epochs = 800
 param_scheduler = [
     dict(
         type='LinearLR', start_factor=1e-3, by_epoch=True, begin=0, end=20),
@@ -71,12 +116,12 @@ custom_hooks = [dict(type='BasicVisualizationHook', interval=5)]
 find_unused_parameter=False
 
 # Test Scripts
-# visualizer = dict(
-#     type='ConcatImageVisualizer',
-#     fn_key='img_path',
-#     img_keys=['pred_img'],
-#     bgr2rgb=True)
+visualizer = dict(
+    type='ConcatImageVisualizer',
+    fn_key='img_path',
+    img_keys=['pred_img'],
+    bgr2rgb=True)
 
 
-# custom_hooks = [
-#     dict(type='BasicVisualizationHook', interval=1)]
+custom_hooks = [
+    dict(type='BasicVisualizationHook', interval=1)]
